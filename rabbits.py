7751b620adfc905766b9aa2bdfbf1c3edb07fd55
Kaleb Maravilla <=>
2023-02-14 19:18:27 -0700

    added the function do_research/ changed teens to 0

--- rabbits.py
+++ rabbits.py
@@ -1,21 +1,21 @@
+def do_research(cages, adults, babies):
+    total = 1
+    teens = 0
+    month = 1
 
-adults = 0
-babies = 0
-total = 1
-teens = 1
-month = 1
-cages = 500
-
-while total < cages:
-    print(month, adults + teens, babies, total)
-    adults += teens
-    teens = babies
-    babies = adults
-    total = adults + teens + babies
-    month += 1
-    if total > cages:
+    while total < cages:
+        
         print(month, adults + teens, babies, total)
-        print("cages will run out at month", month)
+        adults += teens
+        teens = babies
+        babies = adults
+        total = adults + teens + babies
+        month += 1
+        if total > cages:
+            print(month, adults + teens, babies, total)
+            print("cages will run out at month", month)
+
+do_research(500, 1, 0)
 
 
 
