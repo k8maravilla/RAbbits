9df175cf28d73ba099f537e5d831343e0e51ca79
Kaleb Maravilla <=>
2023-02-15 17:43:07 -0700

    added list

--- rabbits.py
+++ rabbits.py
@@ -2,20 +2,36 @@ def do_research(cages, adults, babies):
     total = 1
     teens = 0
     month = 1
-
+    my_list = [month, adults + teens, babies, total]
 
     while total < cages: 
-        print(month, adults + teens, babies, total)
+        #write to csv file
         adults += teens
         teens = babies
         babies = adults
         total = adults + teens + babies
         month += 1
+        my_list = [month, adults + teens, babies, total]
+        print(my_list)
         if total > cages:
-            print(month, adults + teens, babies, total)
             print("cages will run out at month", month)
             break
 
+
+
+
+
+
+
+
+
+# my_file = open('run_rabbits.txt', 'w')
+
+# my_list = [
+    # []
+# ]
+
+# my_file.close()
 
 do_research(500,1,0)   
 
